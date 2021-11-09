7d2e9daabf8be4c9a5cdaf3e5f73484db3b0cca3
Nils Helming <nilshelming@gmail.com>
2021-11-09 22:53:26 +0100

    Anfang Aufgabe 2.1

---
+++ Workspace/Aufg2/Textstatistik/main.cpp
@@ -0,0 +1,24 @@
+/*
+ * -----------------------------------------------------------------------------
+ *
+ * Aufgabe: 2.1 Textstatistik
+ * Autor: Nils Helming
+ *
+ * Ausfuehrung der Aufgabe 2.1
+ *
+ * Praktikum Programmierung 3, HS Osnabrueck
+ * -----------------------------------------------------------------------------
+ */
+
+#include <iostream>
+
+/*
+ * Funktion: int main()
+ *
+ *
+ * return           : int           - 0, falls erfolgreich ausgeführt
+ */
+int main(){
+
+    return 0;
+}
