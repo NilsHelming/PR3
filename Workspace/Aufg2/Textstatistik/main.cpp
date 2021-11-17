79f49ab575c8c4003bbeb446b9ccfc8cae46ad6e
Nils Helming <nilshelming@gmail.com>
2021-11-17 09:46:56 +0100

    adsf

--- Workspace/Aufg2/Textstatistik/main.cpp
+++ Workspace/Aufg2/Textstatistik/main.cpp
@@ -14,11 +14,22 @@
 #include <fstream>
 
 #include <map>
+#include <set>
+#include <unordered_set>
 #include <string>
 #include <ctype.h>
 #include <algorithm>
 
+struct lengthHash {
+    size_t operator()(std::string key) const {
+        return key.length();
+    }
+};
+
 typedef std::map<char, int> Charcounter;
+typedef std::unordered_set<std::string, lengthHash> LengthSet;
+typedef std::set<std::string> StringSet;
+
 
 void count_char(Charcounter& counter, std::ifstream& file){
     if(!file.is_open())
@@ -33,12 +44,13 @@ void count_char(Charcounter& counter, std::ifstream& file){
 }
 
 std::string& toWord(std::string& str){
-    str.erase(
-        std::remove_if(
-            str.begin(), str.end(), [](char c){ return !std::isalpha(c); }
+    str.erase( //remove all non-alphabetical characters
+        std::remove_if(str.begin(), str.end(),
+            [](char c){ return !std::isalpha(c); }
         ), str.end());
-    std::transform(str.begin(), str.end(), str.begin(),
-        [](unsigned char c){ return std::tolower(c); });
+    std::transform(str.begin(), str.end(), str.begin(), //tolower(str);
+        [](unsigned char c){ return std::tolower(c); }
+    );
 
     return str;
 }
@@ -47,16 +59,20 @@ bool isWordLength(const std::string& word){
     return (word.length() >= 2 && word.length() <= 20);
 }
 
-void stuffs(std::ifstream& file){
+LengthSet collection_by_length(std::ifstream& file){
     if(!file.is_open())
         throw new std::logic_error("cannot read from a closed file.");
+
+    LengthSet lengthSet;
 
     while(!file.eof()){
         std::string s;
         file >> s;
         if(isWordLength(toWord(s)))
-            std::cout << s.length() << "\t" <<  s << std::endl;
+            lengthSet.insert(s);
     }
+
+    return lengthSet;
 }
 
 
@@ -92,10 +108,29 @@ int main(){
     #if 1 //Test 2:
     {
         std::ifstream reader(filename);
-        stuffs(reader);
+
+
+        LengthSet lengthSet = collection_by_length(reader);
+
+        for(int i=2; i<=20; ++i){
+            int bucketSize = lengthSet.bucket_size(
+                lengthSet.bucket(std::string(i, 'a'))
+            );
+            std::cout << "Strings of length " << i << ": " << bucketSize << std::endl;
+        }
+
+        int nLength;
+
+        std::cout << "Choose a length, to which you want to know every word:" << std::endl;
+        std::cin >> nLength;
+
+        auto bucketId = lengthSet.bucket(std::string(nLength, 'a'));
+
+        for(std::string str : StringSet(lengthSet.begin(bucketId), lengthSet.end(bucketId)))
+            std::cout << str << std::endl;
     }
     #endif
 
-
+    system("pause");
     return 0;
 }
